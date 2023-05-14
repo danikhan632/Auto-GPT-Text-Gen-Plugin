2cc502eef9e7a718abd1ab809be3fe55f3f00dc9
David Delgado <david@sidewaysthought.net>
2023-05-14 17:34:48 -0500

    Re-shape messages so they are a chat format expected by LLaMA-type models

--- src/auto_gpt_text_gen_plugin/client.py
+++ src/auto_gpt_text_gen_plugin/client.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import json
 from colorama import Fore, Style
 from autogpt.logs import logger
 
@@ -14,6 +15,44 @@ class Client:
         #     "api_key": self.api_key 
         # }
 
+    def remove_whitespace(self, text:str) -> str:
+        """
+        Flatten multiple whitespace characters into a single space.
+
+        Args:
+            text (str): The text to remove whitespace from.
+
+        Returns:
+            str: The text with extra whitespace removed.
+        """
+
+        return " ".join(text.split())
+
+
+    def reshape_message(self, messages:list) -> str:
+        """
+        Convert the OpenAI message format to a string that can be used by the API.
+
+        Args:
+            messages (list): List of messages. Defaults to [].
+
+        Returns:
+            str: String representation of the messages.
+        """
+
+        # Prime the variables
+        message_string = ''
+        
+        # Consolidate messages by role
+        for message in messages:
+            # Capitalize the first letter of the name
+            name = message['role'].capitalize()      
+            user_msg = self.remove_whitespace(message['content'])  
+            message_string += f"{name}: {user_msg}\n\n"
+
+        return message_string
+
+
     def create_chat_completion(self, messages, temperature, max_tokens):
         logger.debug(
             f"{Fore.LIGHTRED_EX}Auto-GPT-Text-Gen-Plugin:{Fore.RESET} Creating chat completion with messages {messages}\n"
@@ -24,7 +63,11 @@ class Client:
             max_tokens=400
         if float(temperature)==0.0:
             temperature=0.01
-        request = {'prompt': str(messages),'temperature': float(temperature), 'max_tokens': int(max_tokens)}
+        reshaped_messages = self.reshape_message(messages)
+        logger.debug(
+            f"{Fore.LIGHTRED_EX}Auto-GPT-Text-Gen-Plugin:{Fore.RESET} Reshaped messages to:\n{reshaped_messages}"
+        )
+        request = {'prompt': reshaped_messages, 'temperature': float(temperature), 'max_tokens': int(max_tokens)}
 
         response = requests.post(self.base_url+'/api/v1/generate', json=request)
         
@@ -32,8 +75,14 @@ class Client:
             logger.debug(
                 f"{Fore.LIGHTRED_EX}Auto-GPT-Text-Gen-Plugin:{Fore.RESET} Got response {response.json()}"
             )
-            resp=response.json()
-            return resp['results'][0]['text']
+            response = response.json()
+            resp = response['results'][0]['text']
+            resp = self.remove_whitespace(resp)
+            logger.debug(
+                f"{Fore.LIGHTRED_EX}Auto-GPT-Text-Gen-Plugin:{Fore.RESET} Returning response:\n {resp}\n"
+                f"(with formatted JSON: \n{resp}\n\n"
+            )
+            return resp
         else:
             logger.debug(
                 f"{Fore.LIGHTRED_EX}Auto-GPT-Text-Gen-Plugin:{Fore.RESET}\n"
