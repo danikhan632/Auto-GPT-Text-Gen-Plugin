5ec713d331d8502220f751043641d7511d3704c4
David Delgado <david@sidewaysthought.net>
2023-05-19 22:24:51 -0500

    Initial Monolithic rebuild code--AI profile.

--- src/auto_gpt_text_gen_plugin/monolithic_prompt.py
+++ src/auto_gpt_text_gen_plugin/monolithic_prompt.py
@@ -1,8 +1,5 @@
 import json
-from autogpt.config import Config
-from autogpt.config.ai_config import AIConfig
 from autogpt.logs import logger
-from autogpt.prompts.generator import PromptGenerator
 from .prompt_engine import PromptEngine
 
 class MonolithicPrompt(PromptEngine):
@@ -11,15 +8,8 @@ class MonolithicPrompt(PromptEngine):
         """Initializes the MonolithicPrompt class."""
 
         super().__init__()
-
-        # Initialize variables
         self.prompt_profile = prompt_profile
-        self.prompt_generator = PromptGenerator()
-        self.config = Config()
-        self.ai_config = AIConfig.load(self.config.ai_settings_file)
 
-        self.prompt_profile = prompt_profile
-    
 
     def reshape_message(self, messages:list) -> str:
         """
@@ -32,67 +22,13 @@ class MonolithicPrompt(PromptEngine):
             str: String representation of the messages.
         """
 
+        self.original_system_msg = messages[0]['content']
+
         # Prime the variables
         message_string = ''
-        
-        # Consolidate messages by role
-        for message in messages:
-            # Capitalize the first letter of the name
-            name = message['role'].capitalize()      
-            user_msg = self.remove_whitespace(message['content'])  
-            message_string += f"{name}: {user_msg}\n\n"
 
-        return message_string
-    
-
-    def build_prompt_payload(self, commands:str = '') -> str:
-        """ 
-        Build the prompt for the API.
-
-        Args:
-            commands (str): The commands to add to the prompt. Defaults to ''.
-
-        Returns:
-            str: The prompt to send to the API.
-        """
-
-        prompt_string = ''
-
-        # Build the agent profile
-        prompt_string += self.prompt_profile['strings']['lead_in']
-        prompt_string += self.ai_config.ai_name
-        prompt_string += ', '
-        prompt_string += self.ai_config.ai_role + '\n\n'
-        prompt_string += self.prompt_profile['strings']['goal_label']
-
-        # Goals
-        for i, goal in enumerate(self.ai_config.ai_goals):
-            prompt_string += f"{i+1}. {goal}\n"
-
-        # Build prompt strings
-        for constraint in self.prompt_profile['strings']['constraints']:
-            self.prompt_generator.add_constraint(constraint)
-
-        for resource in self.prompt_profile['strings']['resources']:
-            self.prompt_generator.add_resource(resource)
-
-        for evaluation in self.prompt_profile['strings']['performance_eval']:
-            self.prompt_generator.add_performance_evaluation(evaluation)
-
-        # Assemble
-        prompt_string += self.prompt_profile['strings']['constraints_label']
-        prompt_string += self.prompt_generator._generate_numbered_list(self.prompt_generator.constraints)
-        prompt_string += self.prompt_profile['strings']['commands_label']
-        prompt_string += commands
-        prompt_string += self.prompt_profile['strings']['resources_label']
-        prompt_string += self.prompt_generator._generate_numbered_list(self.prompt_generator.resources)
-        prompt_string += self.prompt_profile['strings']['performance_eval_label']
-        prompt_string += self.prompt_generator._generate_numbered_list(self.prompt_generator.performance_evaluation)
-        prompt_string += self.prompt_profile['strings']['response_format_pre_prompt']
-        prompt_string += json.dumps(self.prompt_profile['strings']['response_format'])
-        prompt_string += self.prompt_profile['strings']['response_format_post_prompt']
-        prompt_string += '\n' + self.prompt_profile['strings']['postscript']
-
-        return prompt_string
+        # Rebuild prompt
+        message_string += self.get_ai_profile()
 
 
+        return str(messages)
--- src/auto_gpt_text_gen_plugin/prompt_engine.py
+++ src/auto_gpt_text_gen_plugin/prompt_engine.py
@@ -1,9 +1,26 @@
 import re
+from autogpt.config import Config
+from autogpt.config.ai_config import AIConfig
+from autogpt.prompts.generator import PromptGenerator
 
 class PromptEngine:
 
     def __init__(self):
-        pass
+
+        # Constants
+        self.USER_NAME = "User: "
+
+        # Pull-in from Auto-GPT
+        self.prompt_generator = PromptGenerator()
+        self.config = Config()
+        self.ai_config = AIConfig.load(self.config.ai_settings_file)
+
+        # Variables
+        self.prompt_profile = {}
+        self.original_system_msg = ''
+
+        # Regular expressions
+        self.regex_os = r'The OS you are running on is:(.*?)\n\nGOALS'
 
 
     def remove_whitespace(self, text:str) -> str:
@@ -18,6 +35,7 @@ class PromptEngine:
         """
 
         return " ".join(text.split())
+    
 
     def extract_commands(self, message = '') -> str:
         """
@@ -40,4 +58,140 @@ class PromptEngine:
             return match
         except AttributeError:
             return ''
+        
+
+    def reshape_message(self, messages:list):
+        """
+        Inhereted method
+        """
+
+        pass
+
+
+    def get_profile_attribute(self, attribute:str, container:str = '') -> str:
+        """
+        Get an attribute from the AI config.
+
+        Args:
+            attribute (str): The attribute to get.
+
+        Returns:
+            str: The attribute's value.
+        """
+
+        if container == '':
+            base = self.prompt_profile
+        else:
+            base = self.prompt_profile[container]
+
+        if base[attribute] is not None:
+            return base[attribute]
+        else:
+            return ''
+
+    
+    def get_agent_name(self) -> str:
+        """
+        Get the agent's name from the AI config.
+
+        Returns:
+            str: The agent's name.
+        """
+
+        return self.ai_config.ai_name
+    
+
+    def get_agent_role(self) -> str:
+        """
+        Get the agent's role from the AI config.
+
+        Returns:
+            str: The agent's role.
+        """
+
+        return self.ai_config.ai_role
+    
+
+    def get_agent_goals(self) -> str:
+        """
+        Get the agent's goals from the AI config.
+
+        Returns:
+            list: The agent's goals.
+        """
+
+        goals_list = ''
+        for i, goal in enumerate(self.ai_config.ai_goals):
+            goals_list += f"{i+1}. {goal}"
+
+        return goals_list
+    
+
+    def get_profile_list_as_line(self, attribute:str, container:str = '') -> str:
+        """
+        Get a list from the profile.
+
+        Args:
+            attribute (str): The attribute to get.
+            container (str, optional): The container to put the list in. Defaults to None.
+
+        Returns:
+            str: The list as a string.
+        """
+
+        response = ''
+
+        if container == '':
+            base = self.prompt_profile
+        else:
+            base = self.prompt_profile[container]
+
+        if attribute in base:
+            for item in base[attribute]:
+                response += item + ' '
+
+        return response
+    
+
+    def extract_from_original(self, regex:str) -> str:
+        """
+        Extract a string from the original system message.
+        
+        Args:
+            regex (str): The regular expression to use.
+            
+        Returns:
+            str: The extracted string.
+        """
+
+        response = ''
+
+        match = re.search(regex, self.original_system_msg, re.DOTALL)
+        if match is not None:
+            response = match.group(1).strip()
+
+        return response
+    
+
+    def get_ai_profile(self) -> str:
+        """
+        Build the AI profile string
+        
+        Returns:
+            str: The AI profile string.
+        """
+
+        response = ''
+
+        response += self.get_profile_attribute('lead_in') + ', '
+        response += self.get_agent_name() + ', '
+        response += self.get_agent_role() + '. '
+        response += self.get_profile_list_as_line('general_guidance', 'strings')
+        response += self.get_profile_attribute('os_prompt', 'strings')
+        response += self.extract_from_original(self.regex_os)
+        response += self.get_profile_attribute('goal_label', 'strings')
+        response += self.get_profile_list_as_line('goals', 'strings')
+        response += self.get_agent_goals()
+
+        return response
 
